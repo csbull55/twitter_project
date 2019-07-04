9116523ea3a303aeefce7d0f18f4db7543224216
Christian <bullcs95@gmail.com>
2019-07-03 21:36:07 -0400

    initial commit

---
+++ tests/test_advanced.py
@@ -0,0 +1,7 @@
+"""
+this will be for more advanced testing of functions
+I don't imagine that this project will get too complex
+but I want to start getting into the habit of structuring projects
+correctly from the start
+instead of getting too deep and having to restructure
+"""
---
+++ tests/test_basic.py
@@ -0,0 +1,8 @@
+"""
+this is for super basic tests
+mainly like one-liners
+or basic functions that I should know
+kinda like adding 2+1
+gotta make sure it works
+"""
+
