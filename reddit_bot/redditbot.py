2cad575899426484999a9b87b1edabea0351661a
Christian <bullcs95@gmail.com>
2019-07-04 10:03:42 -0400

    added subreddit post lookup func

--- reddit_bot/redditbot.py
+++ reddit_bot/redditbot.py
@@ -8,14 +8,21 @@ in the subs that are specified in the subs file
 """
 
 import praw
+from subs_crawl import subs_to_crawl
+
+
+def sub_obj(sub):
+    subreddit_lookup = reddit.subreddit(sub)
+    posts = [submission.title for submission in subreddit_lookup.hot(limit=5)]
+    return posts
+
 
 # creates instance off credentials in praw file
 reddit = praw.Reddit('bot1',
                      user_agent='bot1 user agent',
                      )
 
-subreddit_lookup = reddit.subreddit('all')
-
-posts = [submission.title for submission in subreddit_lookup.hot(limit=10)]
-
-print('\n'.join(posts))
+# returns top posts from subreddits
+for sub in subs_to_crawl():
+    posts = sub_obj(sub)
+    print(sub + '\n' + '\n'.join(posts) + '\n')
---
+++ reddit_bot/subs_crawl.py
@@ -0,0 +1,10 @@
+# this defines the subs to crawl
+
+
+def subs_to_crawl():
+    subs_crawl = [
+        '2007scape',
+        'baseball',
+        'mtb',
+        ]
+    return subs_crawl
