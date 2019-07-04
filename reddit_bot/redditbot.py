8caee41591ef826ee7bcbae4a82f076e1abed660
Christian <bullcs95@gmail.com>
2019-07-03 22:16:43 -0400

    added secrets file

---
+++ reddit_bot/redditbot.py
@@ -0,0 +1,20 @@
+# Christian Bull
+# python3.7
+
+"""
+this is the main file for the reddit bot
+basic tasks is it will reply to comments that meet a certain parameter
+in the subs that are specified in the subs file
+"""
+
+import requests
+import requests.auth
+import praw
+import secrets
+
+print(secrets.user())
+
+# basic info
+
+
+
